89cc7ea567a0c74337bb5f2770db4c9530ba4bbd
zchong <zhangchong@shangquanxiu.com>
2017-05-11 10:48:14 +0800

    1

--- app/utils.py
+++ app/utils.py
@@ -1,2 +1,14 @@
 # -*- coding:utf-8 -*- 
 # author: zchong
+import json
+import uuid
+
+from datetime import datetime, date
+
+def getUUID():
+    str = uuid.uuid1().__str__()
+    list = str.split('-')
+    return (list[0] + list[3]).upper()
+
+if __name__ == '__main__':
+    print(getUUID())
--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-# -*- coding:utf-8 -*- 
-# author: zchong
+import uuid
+
+str = uuid.uuid1().__str__()
+print(str)
+list = str.split('-')
+print(list[0]+list[3])
+
